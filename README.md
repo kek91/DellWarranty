1049fc0080fdd6c7184ea2b9d3abbbd7e82218b6
Teknix <kek@teknix.no>
2017-06-02 15:56:43 +0200

    DellWarranty source

---
+++ README.md
@@ -0,0 +1,29 @@
+# DellWarranty
+
+**Note: Requires a Dell API key**
+
+Simple Electron app for looking up Dell Warranty API and retrieve start/end date for the supplied service tag. I like to have the app open at all times and hide it in a corner on my monitor somewhere, therefore `AlwaysOnTop` is enabled, but you can set it to false in `main.js:22` if desired.
+
+
+
+To build it yourself simply:
+
+- Clone the repo
+- Modify `api.json` with your Dell API key 
+  - `"url"` Change it if your api key is in sandbox mode
+  - `"key"` Your Dell API key
+  - `"user"` Only for visual branding, it serves no purpose for the Dell API
+- Compile it 
+
+
+
+Compiling example using `electron-packager`:
+
+`electron-packager . --platform=win32 --arch=x64 --electronVersion="1.6.2" --icon="Dell.ico" --out="dist" --ignore="\dist" DellWarranty` 
+
+Replace `.` with path to source unless your cwd is already there.
+
+
+
+![Screenshot GUI](DellWarrantyv0.2.5.png)
+
---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const windowStateKeeper = require('electron-window-state');
+
+let mainWindow;
+
+app.on('ready', function () {
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 285,
+        defaultHeight: 320
+    });
+ 
+    mainWindow = new electron.BrowserWindow({
+        'x': mainWindowState.x,
+        'y': mainWindowState.y,
+        'width': mainWindowState.width,
+        'height': mainWindowState.height,
+        title: "DellWarranty",
+        icon: __dirname + '/Dell.png',
+        alwaysOnTop: true
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+ 
+    mainWindowState.manage(mainWindow);
+});
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
